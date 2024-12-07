2ba06fb1024aba4b0eeb88fa5b655e1829d4c4a9
sarj33t <62173187+sarj33t@users.noreply.github.com>
2024-12-07 12:55:30 +0000

    Deploying to gh-pages from @ flutternation/flutternation.github.io@80a36402ffa9d13089ac299c549031c1ee1bf52c 🚀

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,31 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Form submission handler
-    document.getElementById('contact-form').addEventListener('submit', async function(event){
+// Form submission handler
+document.getElementById('contact-form').addEventListener('submit', async function(event){
     
-        // Form data
-        const formData = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            message: document.getElementById('message').value
-        };
+    // Form data
+    const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
 
-        const response = await fetch('https://email.sarj33t.workers.dev/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
-    
-        const result = await response.json();
-
-        if (response.ok) {
-            console.log('Email sent successfully!', result);
-            document.getElementById('form-status').innerHTML = "Your message has been sent successfully!";
-        } else {
-            console.error('Error sending email:', result.message);
-            document.getElementById('form-status').innerHTML = "Failed to send the message. Please try again.";
-        }
+    const response = await fetch('https://email.sarj33t.workers.dev/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
     });
-    
+
+    const result = await response.json();
+
+    if (response.ok) {
+        console.log('Email sent successfully!', result);
+        document.getElementById('form-status').innerHTML = "Your message has been sent successfully!";
+    } else {
+        console.error('Error sending email:', result.message);
+        document.getElementById('form-status').innerHTML = "Failed to send the message. Please try again.";
+    }
 });
